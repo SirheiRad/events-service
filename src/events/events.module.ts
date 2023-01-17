d65dac5a74676125501bf49325210768dc721d56
Siarhei Radkavets <sradkavets@exadel.com>
2023-01-17 19:58:08 +0300

    keep only redis for ws

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Transport, ClientsModule } from '@nestjs/microservices';
 import { EventsController } from './events.controller';
 import { Event, EventSchema } from './schemas/event.schema';
 import { EventsService } from './events.service';
@@ -9,9 +8,6 @@ import { EventsGateway } from './eventsWS.gateway';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
-    ClientsModule.register([
-      { name: 'EVENTS_SERVICE', transport: Transport.TCP },
-    ]),
   ],
   controllers: [EventsController],
   providers: [EventsService, EventsGateway],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './adapters/redis-io.adapter';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const PORT = process.env.PORT;
@@ -10,13 +9,6 @@ async function bootstrap() {
   const redisIoAdapter = new RedisIoAdapter(app);
   await redisIoAdapter.connectToRedis();
   app.useWebSocketAdapter(redisIoAdapter);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { retryAttempts: 5, retryDelay: 3000 },
-  });
-
-  await app.startAllMicroservices();
 
   await app.listen(PORT, () => console.log(`Server strated on port ${PORT}`));
 }
